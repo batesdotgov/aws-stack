8749ea6797a13f9652fb8264c4e7ba4ff792730a
batesdotgov <brian@batesdotgov.com>
2020-04-19 23:50:33 -0400

    removed stress test file and endpoint

--- src/server/app.js
+++ src/server/app.js
@@ -1,29 +1,22 @@
-const express = require('express')
-const path = require('path')
-const knex = require('knex')
-const knexConfig = require('../../knexfile')
+const express = require("express");
+const path = require("path");
+const knex = require("knex");
+const knexConfig = require("../../knexfile");
 
-const db = knex(knexConfig)
-const app = express()
+const db = knex(knexConfig);
+const app = express();
 
-app.get('/', (req, res) => {
-  return res.send('hello world')
-})
+app.get("/", (req, res) => {
+  return res.send("hello world");
+});
 
-app.get('/secret', (req, res) => {
-  return res.send(process.env.SECRET || 'no secret')
-})
+app.get("/secret", (req, res) => {
+  return res.send(process.env.SECRET || "no secret");
+});
 
-app.get('/users', async (req, res) => {
-  const users = await db('users')
-  return res.json({ users })
-})
+app.get("/users", async (req, res) => {
+  const users = await db("users");
+  return res.json({ users });
+});
 
-// # stress test
-app.get('/loaderio-27c1c3d48849223b76e0e32caa0583de.txt', (req, res) => {
-  return res.sendFile(
-    path.join(__dirname, './loaderio-27c1c3d48849223b76e0e32caa0583de.txt'),
-  )
-})
-
-module.exports = app
+module.exports = app;
