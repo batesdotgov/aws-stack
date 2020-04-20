59b311941715a95d60fc8ded02f1d787c79ac4e1
batesdotgov <brian@batesdotgov.com>
2020-04-19 21:42:53 -0400

    added async

--- migrations/20200419213732_seed_users.js
+++ migrations/20200419213732_seed_users.js
@@ -1,11 +1,8 @@
-
-exports.up = function(knex) {
-    return Promise.all([
-        await knex.raw(`insert into users (username, password) values ("user1", "password1")`),
-        await knex.raw(`insert into users (username, password) values ("user2", "password2")`)
-      ]);
+exports.up = async function (knex) {
+  return Promise.all([
+    await knex.raw(`insert into users (username, password) values ("user1", "password1")`),
+    await knex.raw(`insert into users (username, password) values ("user2", "password2")`),
+  ]);
 };
 
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {};
